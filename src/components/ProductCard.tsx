2f1d8bc33d60b718b8871a70e4695ec66904a1d0
Refki Alfarizi <refkialfarizi123@gmail.com>
2024-07-22 21:19:28 +0700

    feat: added images for product

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -20,7 +20,7 @@ const ProductCard = ({
     <div className="bg-white col-span-1 h-[min(220px,_100%)] p-[14px] w-full rounded-xl flex flex-col justify-between">
       <div className="relative self-center">
         <img
-          src="/product-aoka.png"
+          src={`/product/${product["Kode Barang"]}.webp`}
           alt="product"
           className="w-[129px] h-[129px] rounded-xl object-cover"
         />
@@ -30,7 +30,7 @@ const ProductCard = ({
             onClick={() => triggerSubtract(product)}
             className="w-[29px] h-[29px] transition-transform ease-out  bg-red-100 hover:scale-105 rounded-full flex justify-center items-center absolute bottom-0 right-10"
           >
-            <img src="/-.svg" className="w-3 h-3" alt="Subtract item icon" />
+            <img src="/-.svg" className="w-3 h-3" alt="Subtract item icon"/>
           </button>
         )}
 
